d53b6e72c5bcd2ef3eac6b6af417d3d6347c6fb5
abhinav272 <abhinav.ih@gmail.com>
2016-02-27 01:55:10 +0530

    location integrated

--- app/src/main/java/com/abhinav/personalto_dolist/AddToDo.java
+++ app/src/main/java/com/abhinav/personalto_dolist/AddToDo.java
@@ -1,21 +1,35 @@
 package com.abhinav.personalto_dolist;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.database.Cursor;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
 import android.provider.OpenableColumns;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v4.os.ResultReceiver;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -23,6 +37,9 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 import java.util.Calendar;
 
@@ -32,12 +49,17 @@ import java.util.Calendar;
 public class AddToDo extends AppCompatActivity {
 
     private EditText itemName;
-    private TextView itemNotificationSound, itemDate,itemTime, itemLocation;
+    private TextView itemNotificationSound, itemDate, itemTime, itemLocation;
     private Switch isItemSoundEnabled;
+    private LocationManager mlocationManager;
     private static final int PICK_AUDIO_REQ_CODE = 1;
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 122;
     private Uri audioURI;
     private static final String TAG = "AddToDo";
     private Toolbar toolbar;
+    private String address;
+    private AddressResultReceiver addressResultReceiver;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -49,19 +71,19 @@ public class AddToDo extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.todo_save_menu,menu);
+        inflater.inflate(R.menu.todo_save_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(validations()){
+        if (validations()) {
             //todo item to be saved in DB
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void initialize(){
+    private void initialize() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         itemName = (EditText) findViewById(R.id.item_name);
         itemTime = (TextView) findViewById(R.id.item_time);
@@ -80,7 +102,7 @@ public class AddToDo extends AppCompatActivity {
                     @Override
                     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                         monthOfYear++;
-                        itemDate.setText(dayOfMonth+"/"+monthOfYear+"/"+year);
+                        itemDate.setText(dayOfMonth + "/" + monthOfYear + "/" + year);
                     }
                 },
                         Calendar.getInstance().get(Calendar.YEAR),
@@ -96,7 +118,7 @@ public class AddToDo extends AppCompatActivity {
                 TimePickerDialog timePickerDialog = new TimePickerDialog(AddToDo.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        itemTime.setText(hourOfDay+":"+minute);
+                        itemTime.setText(hourOfDay + ":" + minute);
                     }
                 },
                         Calendar.getInstance().get(Calendar.HOUR_OF_DAY),
@@ -108,11 +130,10 @@ public class AddToDo extends AppCompatActivity {
         isItemSoundEnabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
+                if (isChecked) {
                     itemNotificationSound.setText("Pick Alarm Sound");
                     setAlarmPicker();
-                }
-                else{
+                } else {
                     itemNotificationSound.setText("Notification");
                     itemNotificationSound.setOnClickListener(null);
                     Toast.makeText(AddToDo.this, "Alarm Notification disabled", Toast.LENGTH_SHORT).show();
@@ -120,29 +141,50 @@ public class AddToDo extends AppCompatActivity {
             }
         });
 
+        itemLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkPlayServices()) {
+                    mlocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    if (!mlocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                        showGPSDisabledAlertToUser();
+
+                    } else{
+                        showLocationChooserDialog();
+                    }
+                } else
+                    Toast.makeText(AddToDo.this, "Location feature not available on device", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
     }
 
-    private boolean validations(){
-        if(itemName.getText().toString().isEmpty()){
-            Snackbar.make(itemName,"Reminder description is mandatory",Snackbar.LENGTH_LONG).show();
+    private void updateAddressOnUI(String address) {
+        itemLocation.setText(address);
+    }
+
+    private boolean validations() {
+        if (itemName.getText().toString().isEmpty()) {
+            Snackbar.make(itemName, "Reminder description is mandatory", Snackbar.LENGTH_LONG).show();
             return false;
-        }
-        else if(itemTime.getText().toString().isEmpty()){
-            Snackbar.make(itemName,"Reminder time is mandatory",Snackbar.LENGTH_LONG).show();
+        } else if (itemTime.getText().toString().isEmpty()) {
+            Snackbar.make(itemName, "Reminder time is mandatory", Snackbar.LENGTH_LONG).show();
             return false;
         }
 
         return true;
     }
 
-    private void setAlarmPicker(){
+    private void setAlarmPicker() {
         itemNotificationSound.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent audioPickerIntent = new Intent();
                 audioPickerIntent.setType("audio/*");
                 audioPickerIntent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(audioPickerIntent,"Select audio:"),PICK_AUDIO_REQ_CODE);
+                startActivityForResult(Intent.createChooser(audioPickerIntent, "Select audio:"), PICK_AUDIO_REQ_CODE);
             }
         });
     }
@@ -150,7 +192,7 @@ public class AddToDo extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if(requestCode==PICK_AUDIO_REQ_CODE && resultCode==RESULT_OK){
+        if (requestCode == PICK_AUDIO_REQ_CODE && resultCode == RESULT_OK) {
             audioURI = data.getData();
             itemNotificationSound.setText(getFileName(audioURI));
         }
@@ -180,4 +222,99 @@ public class AddToDo extends AppCompatActivity {
         return result;
     }
 
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Toast.makeText(this, "This device does not support google play services.", Toast.LENGTH_LONG).show();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    public void showGPSDisabledAlertToUser() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Location Services Not Active");
+        builder.setMessage("Please enable Location Services and GPS");
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        });
+
+        Dialog alertDialog = builder.create();
+        alertDialog.setCanceledOnTouchOutside(false);
+        alertDialog.show();
+
+    }
+
+    @SuppressLint("ParcelCreator")
+    public class AddressResultReceiver extends ResultReceiver {
+        public AddressResultReceiver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+            if (resultCode == LatLongService.STATUS_PASS) {
+                address = resultData.getString("address");
+                Log.d("Test",address);
+                updateAddressOnUI(address);
+            }
+            super.onReceiveResult(resultCode, resultData);
+        }
+    }
+
+    private void showLocationChooserDialog(){
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle("Choose location");
+        dialogBuilder.setMessage("What location you want to use?");
+        dialogBuilder.setPositiveButton("Current Location", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                addressResultReceiver = new AddressResultReceiver(new Handler());
+                Intent intent = new Intent(AddToDo.this, LatLongService.class);
+                intent.putExtra("receiver",addressResultReceiver);
+                startService(intent);
+            }
+        });
+        dialogBuilder.setNegativeButton("Other", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                userInputAddress();
+            }
+        });
+        Dialog dialog = dialogBuilder.create();
+        dialog.show();
+    }
+
+    private void userInputAddress(){
+        final Dialog dialog = new Dialog(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.custom_dialog_layout,null);
+        dialog.setContentView(view);
+        Button submit = (Button) view.findViewById(R.id.submit);
+        final EditText address = (EditText) view.findViewById(R.id.user_address);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateAddressOnUI(address.getText().toString());
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    @Override
+    protected void onStop() {
+        stopService(new Intent(AddToDo.this, LatLongService.class));
+        super.onStop();
+    }
 }
---
+++ app/src/main/java/com/abhinav/personalto_dolist/LatLongService.java
@@ -0,0 +1,135 @@
+package com.abhinav.personalto_dolist;
+
+import android.app.Service;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.os.ResultReceiver;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by abhinavsharma on 26-02-2016.
+ */
+public class LatLongService extends Service implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private Location mLastLocation;
+    private GoogleApiClient mGoogleApiClient;
+    private Address mAddress = null;
+    private static final String TAG = "LatLongService";
+    public static final int STATUS_PASS = 1;
+    protected ResultReceiver mResultReceiver;
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            mAddress = getAddressFromLatLng(mLastLocation);
+            Log.d(TAG, "" + mAddress);
+            deliverAddressToActivity(STATUS_PASS,mAddress);
+        }
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(TAG, "Location connection failed " + connectionResult.getErrorCode());
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mResultReceiver = intent.getParcelableExtra("receiver");
+        mGoogleApiClient = new GoogleApiClient.Builder(getBaseContext())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private LatLng getLatLongfromAddress(String address) {
+        Geocoder geocoder = new Geocoder(this);
+        List<Address> addressList = null;
+        LatLng latLng;
+
+        try {
+            addressList = geocoder.getFromLocationName(address, 5);
+            if (addressList.size() == 0) {
+                return null;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Address location = addressList.get(0);
+        latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        return latLng;
+
+
+    }
+
+    private Address getAddressFromLatLng(Location mLocation) {
+        Geocoder geocoder;
+        List<Address> addresses;
+        Address address = null;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(mLocation.getLatitude(), mLocation.getLongitude(), 1);
+            address = addresses.get(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return address;
+    }
+
+    private void deliverAddressToActivity(int resultCode, Address mAddress) {
+        String addrs = "";
+        Bundle bundle = new Bundle();
+        if (resultCode == STATUS_PASS) {
+            addrs = mAddress.getAddressLine(0)
+                    + mAddress.getAddressLine(1)
+                    + mAddress.getAddressLine(2)
+                    + mAddress.getAddressLine(3);
+            bundle.putString("address", addrs);
+        }
+        mResultReceiver.send(resultCode, bundle);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "Service Stopped");
+        super.onDestroy();
+    }
+}
